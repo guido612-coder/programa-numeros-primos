7185403786391829b7aa39025cfa9755d2e64bdb
guido612-coder <guidomarquez612@gmail.com>
2026-09-10 21:30:53 -0300

    Módulo para funciones de números primos

---
+++ primo.h
@@ -0,0 +1,10 @@
+#ifndef PRIMO_H
+#define PRIMO_H
+
+// Función para verificar si un número es primo
+int es_primo(int num);
+
+// Función para verificar si un dígito es primo
+int es_digito_primo(char digito);
+
+#endif
